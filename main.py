5dd1e8de94e8e24c34156e8f08f8d8a605275223
MartianSpaceFox <59034382+MartianSpaceFox@users.noreply.github.com>
2021-06-11 16:44:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+# Date: 06/11/2021
+#
+# Project: Team 8 Payload Software Package for NASA L'SPACE LUCY Internship Summer 2021
+# Github Project Url: https://github.com/users/lexislynn/projects/1
+#
+# Contributors: Alexis Warren, Lead Software Engineer;
+# Adriana Bonilla, Software Engineer 2;
+# Jessica Nachtman, Systems and ATLO Engineer and Software Engineer 3.
+#
+#
+
+
+
+
+
+
+
+
